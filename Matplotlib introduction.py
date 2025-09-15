4d5207d30a12499a190db5c750ac147d755369c6
Hdez_J <jlopezhdez036@gmail.com>
2025-09-15 19:01:10 +1000

    lineal_plot

--- Matplotlib introduction.py
+++ Matplotlib introduction.py
@@ -20,6 +20,7 @@ Explanatory data visualization:
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 df_dowjones = sns.load_dataset('dowjones') 
@@ -27,23 +28,65 @@ df_dowjones = sns.load_dataset('dowjones')
 df_penguins = sns.load_dataset('penguins')
 # https://github.com/allisonhorst/palmerpenguins
 
+###############################################
 # Create simple line plot
 
-df.info()
+df_dowjones.info()
 
-plt.plot(df.body_mass_g);
+plt.plot(df_dowjones.Date, df_dowjones.Price);
 
-plt.plot()
+# Add title and labels
 
-# Add Title, labels
-
-plt.plot(ages,mib_salary)
-plt.xlabel('$Ages$')
-plt.ylabel('$Median Salary (USD)$')
-plt.title('$Median Salary (USD) by Age$')
-plt.show()
+plt.plot(df_dowjones.Date, df_dowjones.Price)
+plt.xlabel('$Year$')
+plt.ylabel('$Price$')
+plt.title('Dow-Jones Industrial Stock Price Index for United States')
 
 
 # Comparison
 
-# Microbiolog
+df_dowjones.shape
+
+df_dowjones['Error1'] = df_dowjones.Price + 100
+df_dowjones['Error2'] = df_dowjones.Price - 100
+
+plt.plot(df_dowjones.Date, df_dowjones.Price, label = 'Original price')
+plt.plot(df_dowjones.Date, df_dowjones.Error1, label = 'Positive margin')
+plt.plot(df_dowjones.Date, df_dowjones.Error2, label = 'Negative margin')
+plt.xlabel('$Year$')
+plt.ylabel('$Price$')
+plt.title('Dow-Jones Industrial Stock Price Index for United States')
+plt.tight_layout() # Check info: padding (almohadillas, margenes )
+plt.legend(loc = 'upper left') # Check info
+
+# Customization
+
+plt.plot(df_dowjones.Date, df_dowjones.Error1, label = 'Positive margin', 
+         color = 'midnightblue', linewidth = .5, linestyle = '--')
+plt.plot(df_dowjones.Date, df_dowjones.Error2, label = 'Negative margin', 
+         color = 'midnightblue', linewidth = .5, linestyle = '--')
+plt.plot(df_dowjones.Date, df_dowjones.Price, label = 'Original price',
+         color = 'darkorchid', linewidth = 1)
+plt.xlabel('$Year$')
+plt.ylabel('$Price$')
+plt.title('Dow-Jones Industrial Stock Price Index for United States')
+plt.legend(loc = 'upper left') # Check info
+
+# Hex color: check!
+
+# Font size
+
+plt.plot(df_dowjones.Date, df_dowjones.Error1, label = 'Positive margin', 
+         color = 'midnightblue', linewidth = .5, linestyle = '--')
+plt.xlabel('$Year$', fontsize = 25)
+plt.ylabel('$Price$', fontsize = 12)
+plt.title('Dow-Jones Industrial Stock Price Index for US', fontsize = 12)
+
+
+plt.figure(figsize= (8,4))
+plt.plot(df_dowjones.Date, df_dowjones.Error1, label = 'Positive margin', 
+         color = 'midnightblue', linewidth = .5, linestyle = '--')
+plt.xlabel('$Year$')
+plt.ylabel('$Price$')
+plt.title('Dow-Jones Industrial Stock Price Index for US')
+plt.grid(linestyle='--')
